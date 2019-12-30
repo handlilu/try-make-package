f36a73cc05f4340fa9226159b6e11b9c66948c88
lilu <lilu_12611@126.com>
2019-12-30 16:51:53 +0800

    init && dev package

---
+++ src/Flower/Lily.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by sub.
+ * User: lilu
+ * Date: 2019/12/30
+ * Time: 21:39
+ */
+
+namespace Flower\Lily;
+
+class Lily
+{
+    public function desc()
+    {
+        echo "this is rose flower";
+    }
+}
---
+++ src/Flower/Rose.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by sub.
+ * User: lilu
+ * Date: 2019/12/30
+ * Time: 21:39
+ */
+
+namespace Flower\Rose;
+
+class Rose
+{
+    public function desc()
+    {
+        echo "this is rose flower";
+    }
+}
